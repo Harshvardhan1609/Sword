8a58bd45a28ba238ba4980c01ea873b70ff35a99
HARSHVARDHAN PUROHIT <harshvardhanpurohit2020@gmail.com>
2023-04-30 11:19:09 +0530

    Text Summerizer

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        dmserif : ["DM Serif Display", "serif"],
+        spline : ["Spline Sans Mono", "monospace"]
+      }
+    },
+  },
+  plugins: [],
+}
